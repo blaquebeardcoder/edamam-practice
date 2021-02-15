bde3ed8a8c0f50f31fefebfbbf7182e4eec360e3
Jahaziel Israel <blaquebeardcoder@gmail.com>
2021-02-15 02:29:56 -0500

    completed project with help from the squad

---
+++ edamam-test-project/edamam-project/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>Yumzie Copyright 2021. All Rights Reserved.</h1>
+        </div>
+    );
+}
+
+export default Footer
